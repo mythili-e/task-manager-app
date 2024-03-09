ec930089892da9d19fc2bda9459046d103dfde16
mythili-e <mythili.eniyarasu@gmail.com>
2024-03-09 19:13:52 +0530

    code cleanup

--- src/App.js
+++ src/App.js
@@ -28,53 +28,47 @@ function App() {
     setShowModal(false);
   }
 
-   return(
+  return(
     <>
       <div className="text-center">
         <h1>Task Manager</h1>
-      <button onClick={addTask}>Add Task</button>
+        <button onClick={addTask}>Add Task</button>
       
-      <Modal show={showModal} onHide={handleClose}>  
-        <Modal.Header closeButton>
-          <Modal.Title>Add Task</Modal.Title>
-        </Modal.Header>  
-        <Modal.Body>
-          <Form onSubmit={handleSubmit} >
-            <Form.Group className="mb-3">
-              <Form.Label>Task Name:</Form.Label>
-              <Form.Control  type="text" name="name" placeholder="Enter Name" onChange={handleChange} />    
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Label>Date:</Form.Label>
-              <Form.Control  type="text" name="date" placeholder="Enter Date" onChange={handleChange} />    
-            </Form.Group>
-            <div className="text-center">
-              <Button variant="primary" type="submit">
-                  Save
-              </Button>
-            </div>
-          </Form>
-        </Modal.Body>
-      </Modal>
-      {/* <Form >
-        
-        
-        
-      </Form> */}
-
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>S.No</th>
-            <th>Name</th>
-            <th>Date</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-      </Table>
+        <Modal show={showModal} onHide={handleClose}>  
+          <Modal.Header closeButton>
+            <Modal.Title>Add Task</Modal.Title>
+          </Modal.Header>  
+          <Modal.Body>
+            <Form onSubmit={handleSubmit} >
+              <Form.Group className="mb-3">
+                <Form.Label>Task Name:</Form.Label>
+                <Form.Control  type="text" name="name" placeholder="Enter Name" onChange={handleChange} />    
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Date:</Form.Label>
+                <Form.Control  type="text" name="date" placeholder="Enter Date" onChange={handleChange} />    
+              </Form.Group>
+              <div className="text-center">
+                <Button variant="primary" type="submit">
+                    Save
+                </Button>
+              </div>
+            </Form>
+          </Modal.Body>
+        </Modal>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>S.No</th>
+              <th>Name</th>
+              <th>Date</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+        </Table>
       </div>
-      </>
-);
+    </>
+  );
 }
   
 export default App;
